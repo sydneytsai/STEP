e0969a5ee0785849f6e794158f3bcc811c01b9e3
Sydney Tsai <sydneytsai@berkeley.edu>
2023-09-22 12:13:38 -0700

    debug

--- distribution.py
+++ distribution.py
@@ -5,8 +5,8 @@ gc = gspread.oauth()
 distribution_sheet = gc.open_by_key('1nFSEMI7KSdQ7z4vVSrc-tyIBuUE8bdNo-hsOBEy-qB4')
 application_sheet = gc.open_by_key('1RgTqIQNlmTzLSNYf65uGF3iUaFgwCPPG9owc2GSU_TQ') 
 
-distribution_worksheet = distribution_sheet.get_worksheet(0)
-application_worksheet = application_sheet.get_worksheet(0)
+distribution_worksheet = distribution_sheet.worksheet("Fall 2023")
+application_worksheet = application_sheet.worksheet("DATABASE")
 
 dist_rows = distribution_worksheet.get_all_values()
 app_rows = application_worksheet.get_all_values()
@@ -17,7 +17,10 @@ for dist_row in dist_rows:
     if (dist_row[7] == 'Pending'):
         email = dist_row[1]
         temp = application_worksheet.find(email, in_row=None, in_column=None, case_sensitive=False)
-        app_row = app_rows[temp.row-1]
+        if (temp):
+            app_row = app_rows[temp.row-1]
+        else:
+            print(f"Error:{email}")
         collected = True
         for item in app_row:
             if 'Pending' in item:
